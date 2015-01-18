dd9aba8a67b1da9eb1e350a9977a9c0e48798241
Twisted Pear <TwistedPear-AT@users.noreply.github.com>
2015-01-18 12:53:31 +0000

    Add /commands endpoint

--- pearweb.py
+++ pearweb.py
@@ -4,25 +4,36 @@
 """
 pearweb.py
 
-The (exceedingly simple) web page for the PearBot IRC bot.
+The (exceedingly simple) web page for the PearBot IRC golem.
 
 Copyright (c) 2015 Twisted Pear <pear at twistedpear dot at>
 See the file LICENSE for copying permission.
 """
 
 import asyncio
+import json
 import logging
 import signal
+
 from aiohttp import web
+from mako.lookup import TemplateLookup
 from os import getenv
 
 LOG_FORMAT = "{asctime} [{process}] {levelname}({name}): {message}"
 logging.basicConfig(level=logging.INFO, format=LOG_FORMAT, style="{")
 
+lookup = TemplateLookup(directories=["templates"], output_encoding="utf-8")
+
+with open("commands.json") as cmd_fp:
+    commands = json.load(cmd_fp)
+
 
 @asyncio.coroutine
-def handle_root(request):
-    return web.Response(text="Hello, World!")
+def handle_commands(request):
+    template = lookup.get_template("commands.mako")
+    body = template.render(subtitle="Commands", commands=commands)
+    return web.Response(body=body)
+
 
 if __name__ == "__main__":
     logger = logging.getLogger("pearweb")
@@ -30,7 +41,7 @@ if __name__ == "__main__":
 
     logger.info("Setting up application routes...")
     app = web.Application()
-    app.router.add_route("GET", "/", handle_root)
+    app.router.add_route("GET", "/commands", handle_commands)
 
     port = int(getenv("PORT", 5000))
     logger.info("Creating server on port {port}...".format(port=port))
---
+++ templates/base.mako
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Pump19 &#124; ${subtitle}</title>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+  </head>
+  <body>
+  <div class="container-fluid">
+    <header class="page-header">
+    <h1>Pump19 Twitch Chat Golem</h1>
+    </header>
+    ${self.body()}
+  </div>
+  </body>
+</html>
